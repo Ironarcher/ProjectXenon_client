e6fc6a10770511d41135b9769d11c1c5ec885b3e
Ironarcher <arpad.kovesdy@gmail.com>
2014-12-20 13:35:32 -0600

    Added basic networking to send strings

--- Curry_Client/Form1.cs
+++ Curry_Client/Form1.cs
@@ -7,14 +7,241 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Runtime.InteropServices;
 
 namespace Curry_Client
 {
+
     public partial class Form1 : Form
     {
+        private const int port = 32320;
+        // ManualResetEvent instances signal completion.
+        private static ManualResetEvent connectDone =
+            new ManualResetEvent(false);
+        private static ManualResetEvent sendDone =
+            new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone =
+            new ManualResetEvent(false);
+
+        // The response from the remote device.
+        private static String response = String.Empty;
+
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+        /*
+        public void textWriteLine(String t)
+        {
+            String[] oldAr = {};
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new MethodInvoker(delegate { oldAr = richTextBox1.Lines; }));
+            }
+            else
+            {
+                oldAr = richTextBox1.Lines;
+            }
+
+            if(oldAr.Length > 0){
+                String[] newAr = new String[oldAr.Length + 1];
+                for (int i = 0; i < oldAr.Length; i++)
+                    newAr[i + 1] = oldAr[i];
+
+                newAr[0] = t;
+                if (richTextBox1.InvokeRequired)
+                {
+                    richTextBox1.Invoke(new MethodInvoker(delegate { richTextBox1.Lines = newAr; }));
+                }
+                else
+                {
+                    richTextBox1.Lines = newAr;
+                }
+            }
+        }
+         */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+            Console.WriteLine("Client Launch");
+            //connect("127.0.0.1");
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        //IP_AD is the IP address of the server
+        private static void connect(string IP_AD)
+        {
+            try
+            {
+                //init
+                IPAddress ipAddress = IPAddress.Parse(IP_AD);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+                Socket client = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                //Connect through the remote socket (endpoint)
+                client.BeginConnect(remoteEP,
+                    new AsyncCallback(ConnectCallback), client);
+
+                // Send test data to the remote device.
+                Send(client, "This is a test<EOF>");
+                sendDone.WaitOne();
+
+                // Receive the response from the remote device.
+                Receive(client);
+                receiveDone.WaitOne();
+
+                // Write the response to the console.
+                Console.WriteLine("Response received : " + response);
+
+                // Release the socket.
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket client = (Socket)ar.AsyncState;
+
+                // Complete the connection.
+                client.EndConnect(ar);
+
+                Console.WriteLine("Socket connected to " + client.RemoteEndPoint.ToString());
+
+                // Signal that the connection has been made.
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                String p = e.ToString();
+                Console.WriteLine(p);
+            }
+        }
+
+        private static void Receive(Socket client)
+        {
+            try
+            {
+                // Create the state object.
+                StateObject state = new StateObject();
+                state.workSocket = client;
+
+                // Begin receiving the data from the remote device.
+                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), state);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                StateObject state = (StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+
+                // Read data from the remote device.
+                int bytesRead = client.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    // There might be more data, so store the data received so far.
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                    // Get the rest of the data.
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                        new AsyncCallback(ReceiveCallback), state);
+                }
+                else
+                {
+                    // All the data has arrived; put it in response.
+                    if (state.sb.Length > 1)
+                    {
+                        response = state.sb.ToString();
+                    }
+                    // Signal that all bytes have been received.
+                    receiveDone.Set();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Send(Socket client, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.
+            client.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), client);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket client = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("Sent " + bytesSent + " bytes to server.");
+
+                // Signal that all bytes have been sent.
+                sendDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        // State object for receiving data from remote device.
+        public class StateObject
+        {
+            // Client socket.
+            public Socket workSocket = null;
+            // Size of receive buffer.
+            public const int BufferSize = 256;
+            // Receive buffer.
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect("127.0.0.1");
+        }
+
+      
     }
 }
