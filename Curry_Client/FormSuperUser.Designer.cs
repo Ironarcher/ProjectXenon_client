f292122e8d52f4f4c4170e9ae80a8de8028d03e8
Warblish <jstafford17@lausanneschool.com>
2014-12-23 17:59:39 -0600

    Started SuperUser UI

--- Curry_Client/FormSuperUser.Designer.cs
+++ Curry_Client/FormSuperUser.Designer.cs
@@ -29,26 +29,168 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(209, 227);
+            this.button1.Location = new System.Drawing.Point(82, 174);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.Size = new System.Drawing.Size(135, 36);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Create Mission";
+            this.button1.Text = "Create New Mission";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(588, 330);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.listBox2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(580, 304);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Groups";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(88, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Delete Group";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "New Group";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(198, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Remove -->";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<-- Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(329, 57);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(185, 173);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(7, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(185, 173);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Groups";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(580, 304);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Missions";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // FormSuperUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(612, 354);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
             this.Name = "FormSuperUser";
             this.Text = "FormSuperUser";
+            this.Load += new System.EventHandler(this.FormSuperUser_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +198,17 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button5;
     }
 }
--- Curry_Client/FormSuperUser.cs
+++ Curry_Client/FormSuperUser.cs
@@ -12,6 +12,8 @@ namespace Curry_Client
 {
     public partial class FormSuperUser : Form
     {
+        public Group selectedGroup;
+        public List<Group> groupList = new List<Group>();
         public FormSuperUser()
         {
             InitializeComponent();
@@ -21,6 +23,146 @@ namespace Curry_Client
         {
             FormMission f = new FormMission();
             f.Show();
+        }
+
+        private void FormSuperUser_Load(object sender, EventArgs e)
+        {
+            //listBox2.Items
+            //GET USERS FROM SERVER
+            //ADD TO LISTBOX 2
+            //GET GROUPS MADE IN CURRICULUM
+            //ADD THESE GROUPS TO GROUPLIST
+            //ADD THE MEMBERS OF DIFFERENT GROUPS TO LISTBOX 1
+            //Default Groups: All Users, All Superusers, 
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox1.Text == "Group Name")
+            {
+                bool flag = true;
+                int i = 1;
+                while (flag)
+                {
+                    bool flag2 = true;
+                    for (int j = 0; j < groupList.Count; j++)
+                    {
+                        if (groupList[j].name == "NewGroup" + i)
+                        {
+                            flag2 = false;
+                        }
+                    }
+                    if (flag2)
+                    {
+                        flag = false;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                Group newg = new Group("NewGroup" + i);
+                groupList.Add(newg);
+                comboBox1.Items.Add(newg.name);
+            }
+            else
+            {
+                bool flag2 = true;
+                for (int j = 0; j < groupList.Count; j++)
+                {
+                    if (groupList[j].name == textBox1.Text)
+                    {
+                        flag2 = false;
+                    }
+
+                }
+                if (flag2)
+                {
+                    Group newg = new Group(textBox1.Text);
+                    groupList.Add(newg);
+                    comboBox1.Items.Add(newg.name);
+                }
+                else
+                {
+                    MessageBox.Show("Error: Group already made with given name");
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete this group?", "Alert", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    String n = comboBox1.SelectedItem.ToString();
+                    comboBox1.Items.Remove(comboBox1.SelectedItem);
+                    for (int i = 0; i < groupList.Count; i++)
+                    {
+                        if (groupList[i].name == n)
+                        {
+                            groupList.Remove(groupList[i]);
+                            updateListBox1();
+                            break;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No group selected", "Alert");
+            }
+        }
+        public void updateListBox1()
+        {
+            if (comboBox1.SelectedItem == null && comboBox1.Items.Count != 0)
+            {
+                comboBox1.SelectedItem = comboBox1.Items[0];
+            }
+            if (comboBox1.SelectedItem != null)
+            {
+                Group selectedG = null;
+                for (int i = 0; i < groupList.Count; i++)
+                {
+                    if (groupList[i].name == comboBox1.SelectedItem.ToString())
+                    {
+                        selectedG = groupList[i];
+                    }
+                }
+                if (selectedG != null)
+                {
+                    listBox1.Items.Clear();
+                    for (int i = 0; i < selectedG.users.Count; i++)
+                    {
+                       // listBox1.Items.Add(User.getFirstName(selectedG.users[i]).ToString() + " " + User.getLastName(selectedG.users[i].ToString()));
+                        //Above line adds users to the listbox
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Group
+    {
+        public string name;
+        public List<int> users = new List<int>();
+        public Group(string n)
+        {
+            name = n;
         }
     }
 }
